f9cc2383909ad474c63a80fbab9b9f6cd7ed960c
Theonrex <theonrex1@gmail.com>
2022-07-20 19:05:52 +0100

    changes

--- js/greensock.js
+++ js/greensock.js
@@ -1,42 +1,25 @@
-let tl = gsap.timeline({
-    // yes, we can add it to an entire timeline!
-    scrollTrigger: {
-      trigger: ".myAmination ",
-      pin: true,   // pin the trigger element while active
-      start: "top top", // when the top of the trigger hits the top of the viewport
-      end: "+=200", // end after scrolling 500px beyond the start
-      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-      snap: {
-        snapTo: "labels", // snap to the closest label in the timeline
-        duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-        delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-        ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-      }
-    }
-  });
-
-
-  let xl = gsap.timeline({
-    // yes, we can add it to an entire timeline!
-    scrollTrigger: {
-      trigger: ".AminationTwo ",
-      pin: true,   // pin the trigger element while active
-      start: "top top", // when the top of the trigger hits the top of the viewport
-      end: "+=200", // end after scrolling 500px beyond the start
-      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-      snap: {
-        snapTo: "labels", // snap to the closest label in the timeline
-        duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-        delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-        ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-      }
-    }
-  });
-
-//     let xz = gsap.timeline({
+// let tl = gsap.timeline({
 //     // yes, we can add it to an entire timeline!
 //     scrollTrigger: {
-//       trigger: ".AminationThree ",
+//       trigger: ".myAmination ",
+//       pin: true,   // pin the trigger element while active
+//       start: "top top", // when the top of the trigger hits the top of the viewport
+//       end: "+=600", // end after scrolling 500px beyond the start
+//       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//       snap: {
+//         snapTo: "labels", // snap to the closest label in the timeline
+//         duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+//         delay: 0, // wait 0.2 seconds from the last scroll event before doing the snapping
+//         ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+//       }
+//     }
+//   });
+
+
+//   let xl = gsap.timeline({
+//     // yes, we can add it to an entire timeline!
+//     scrollTrigger: {
+//       trigger: ".AminationTwo ",
 //       pin: true,   // pin the trigger element while active
 //       start: "top top", // when the top of the trigger hits the top of the viewport
 //       end: "+=200", // end after scrolling 500px beyond the start
@@ -49,6 +32,23 @@ let tl = gsap.timeline({
 //       }
 //     }
 //   });
+
+// //     let xz = gsap.timeline({
+// //     // yes, we can add it to an entire timeline!
+// //     scrollTrigger: {
+// //       trigger: ".AminationThree ",
+// //       pin: true,   // pin the trigger element while active
+// //       start: "top top", // when the top of the trigger hits the top of the viewport
+// //       end: "+=200", // end after scrolling 500px beyond the start
+// //       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+// //       snap: {
+// //         snapTo: "labels", // snap to the closest label in the timeline
+// //         duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+// //         delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+// //         ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+// //       }
+// //     }
+// //   });
 
 
 
